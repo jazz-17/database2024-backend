692ef7a61a53bb989e856ce6a3135c22d1910a0b
kevinqc-sys <146663297+Hernando-sys@users.noreply.github.com>
2024-05-22 16:31:51 -0500

    removed node_modules from repository

--- routes.js
+++ routes.js
@@ -1,11 +1,23 @@
+import path from "path";
+import express from "express";
+import { fileURLToPath } from "url";
+
+// Get the __dirname equivalent for ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 function setupRoutes(app) {
-    // app.get("/", (req, res) => {
-    //   res.send(process.env.MESSAGE || "Hello World!");
-    // });
+  app.use(express.static(path.join(__dirname, "public")));
 
-    // Add more routes here as needed
-    
-  }
-  
-  export default setupRoutes;
+  // Add more routes here as needed
+  app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+  });
+  app.get("/api/data", (req, res) => {
+    let manager = new DatabaseManager(dbConfig);
+    let result = manager.fetchJsonAndSendToFronted();
+    result.then((result) => res.json({ message: result }));
+  });
+}
+
+export default setupRoutes;
--- server.js
+++ server.js
@@ -1,40 +1,20 @@
 import express from "express";
 import oracle from "oracledb";
-
-import path from "path";
-
-import dotenv from "dotenv";
-dotenv.config();
 import DatabaseManager from "./database/manager.js";
 import setupRoutes from "./routes.js";
-import { fileURLToPath } from "url";
 
 // Connection settings
 const dbConfig = {
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  connectString: process.env.CON_STRING,
+  user: "Prueba",
+  password: "123",
+  connectString: "localhost/xepdb1",
 };
-// Get the __dirname equivalent for ES modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 class Server {
   constructor() {
     this.app = express();
-    this.port = 3000;
-    this.app.listen(this.port, () => {
-      console.log(`Server is running at http://localhost:${this.port}`);
-    });
-    this.app.use(express.static(path.join(__dirname, "public")));
-
-    this.app.get("/", (req, res) => {
-      res.sendFile(path.join(__dirname, "public", "index.html"));
-    });
-    this.app.get("/api/data", (req, res) => {
-      let manager = new DatabaseManager(dbConfig);
-      let result = manager.fetchJsonAndSendToFronted();
-      result.then((result) => res.json({ message: result }));
+    this.app.listen(3000, () => {
+      console.log(`Server is running at http://localhost:3000`);
     });
     this.startDatabaseConn();
     setupRoutes(this.app);
